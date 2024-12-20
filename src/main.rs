4d68db11646e6b976d10045495b92ceb285a524f
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-12-19 20:46:27 -0500

    basic separation in function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
+use reqwest::Client;
+
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let client = reqwest::Client::new();
-    let body = client
-        .get("http://ollama.local/api/tags")
+async fn main() {
+    let client = Client::new();
+    let image = "";
+
+    let image_analysis = analyze_image(client, image).await.unwrap();
+
+    println!("{image_analysis:?}");
+}
+
+async fn analyze_image(client: Client, image: &str) -> Result<String, reqwest::Error> {
+    let res = client
+        .post("http://ollama.local/api/generate")
+        .json(&serde_json::json!({
+            "model":"llama3.2-vision",
+            "prompt": "What is in this picture?",
+            "stream": false,
+            "images": [image]
+        }))
         .send()
         .await?
         .text()
         .await?;
-
-    println!("{body:?}");
-    Ok(())
+    Ok(res)
 }
