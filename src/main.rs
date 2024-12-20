d1ad25ab11e16ae023e0fbbc35a211d1a8427eee
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-12-19 21:44:13 -0500

    working basic parsing

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
 use reqwest::Client;
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
     let client = Client::new();
     let image = "";
 
-    let image_analysis = analyze_image(client, image).await.unwrap();
-
-    println!("{image_analysis:?}");
+    match analyze_image(client, image).await {
+        Ok(response) => println!("{response}"),
+        Err(e) => println!("Error: {e}"),
+    }
 }
 
 async fn analyze_image(client: Client, image: &str) -> Result<String, reqwest::Error> {
-    let res = client
+    let response = client
         .post("http://ollama.local/api/generate")
-        .json(&serde_json::json!({
-            "model":"llama3.2-vision",
-            "prompt": "What is in this picture?",
-            "stream": false,
-            "images": [image]
-        }))
+        .json(&json!(
+            {
+                "model":"llama3.2-vision",
+                "prompt": "What is in this picture?",
+                "stream": false,
+                "images": [image]
+            }
+        ))
         .send()
-        .await?
-        .text()
         .await?;
-    Ok(res)
+
+    let json_response: Value = response.json().await?;
+    Ok(json_response["response"]
+        .as_str()
+        .unwrap_or_default()
+        .to_string())
 }
